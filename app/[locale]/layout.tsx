56c5abce9239a9d8ddb84ca50272382e4775652e
Your Name <you@example.com>
2026-01-11 20:56:28 +0100

    Remove NextIntlClientProvider from layout to fix home page loading

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,22 +1,14 @@
 import type { Metadata } from 'next'
-import { NextIntlClientProvider } from 'next-intl'
-import { getMessages } from 'next-intl/server'
 
 export const metadata: Metadata = {
   title: 'Privora10',
   description: 'Secure Multi-User Encrypted File Transfer Web Application',
 }
 
-export default async function LocaleLayout({
+export default function LocaleLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const messages = await getMessages()
-
-  return (
-    <NextIntlClientProvider messages={messages}>
-      {children}
-    </NextIntlClientProvider>
-  )
+  return children
 }
